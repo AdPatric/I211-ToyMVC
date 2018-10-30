b0c59b70d58f85415527ba257f299a6fb0b4744a
AdamP <adammpatrick@yahoo.com>
2018-10-30 10:15:44 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * Author: Louie Zhu
+ * Date: 3/15/2016
+ * Name: index.php
+ * Description: the homepage
+ */
+require 'autoloader.class.php';
---
+++ toy_view.class.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML">
+<html>
+    <head>
+        <title>ToyMVC List All Toys</title>
+        <link type="text/css" rel="stylesheet" href="includes/style.css" />
+    </head>
+    <body>
+        <h2>Toys in our inventory</h2>
+        <table border="0">
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Price</th>
+            </tr>
+			<?php
+			//add code here to create a new row for each toy
+			
+			?>
+		</table>
+    </body>
+</html>
